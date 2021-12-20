8b43c715f0bad5661083da61f05feb0196ee4dee
jzzdsb <jzz835512239@gmail.com>
2021-12-20 22:09:50 +0800

    代码提交-->构建项目

---
+++ src.main/java/org/example/dto/StudentDto.java
@@ -0,0 +1,10 @@
+package org.example.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentDto {
+    private String id;
+    private String name;
+    private Integer age;
+}
---
+++ src.main/test/org/example/TreeTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class TreeTest extends TestCase {
+
+
+    public void test(){
+        System.out.println("hello world");
+    }
+}
